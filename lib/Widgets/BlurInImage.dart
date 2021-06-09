b7e17197db28cda374108b680f70fda542318c13
Kishan-dhankecha <Dhankechakishan@gmail.com>
2021-06-09 15:29:43 +0530

    BlurHash removed

--- lib/Widgets/BlurInImage.dart
+++ lib/Widgets/BlurInImage.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class BlurInImage extends StatelessWidget {
   final String url;
-  final String blurHash;
 
-  const BlurInImage(
-    this.url, {
-    this.blurHash,
-  });
+  const BlurInImage(this.url);
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +13,17 @@ class BlurInImage extends StatelessWidget {
       loadingBuilder: (BuildContext context, Widget child,
           ImageChunkEvent loadingProgress) {
         if (loadingProgress == null) return child;
-        return Stack(
-          children: [
-            BlurHash(hash: blurHash),
-            Center(
-              child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes
-                    : null,
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  Colors.white38,
-                ),
-                strokeWidth: 1,
-              ),
+        return Center(
+          child: CircularProgressIndicator(
+            value: loadingProgress.expectedTotalBytes != null
+                ? loadingProgress.cumulativeBytesLoaded /
+                    loadingProgress.expectedTotalBytes
+                : null,
+            valueColor: AlwaysStoppedAnimation<Color>(
+              Colors.white38,
             ),
-          ],
+            strokeWidth: 1,
+          ),
         );
       },
     );
